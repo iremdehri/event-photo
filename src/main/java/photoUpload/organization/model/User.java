9256d4fd1d3297b74088f7134a1e80879b6c3740
Hatice İrem Dehri <96865791+iremdehri@users.noreply.github.com>
2026-04-08 23:20:27 +0300

    Add column annotations for fullName and phoneNumber
    Updated column names for better database compatibility.

--- src/main/java/photoUpload/organization/model/User.java
+++ src/main/java/photoUpload/organization/model/User.java
@@ -15,9 +15,11 @@ public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "full_name")
     private String fullName;
     private String email;
     private String password; // Oturum açmak için
+    @Column(name = "phone_number")
     private String phoneNumber;
 
     //Soft Delete
